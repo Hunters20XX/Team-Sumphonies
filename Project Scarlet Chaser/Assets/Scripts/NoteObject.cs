f281efaace650d6074ac9ac3920b8db447714a57
Hunters20XX <MALAM1122@stu.hostos.cuny.edu>
2023-03-16 18:12:15 -0400

    Combo Special + Rank System

--- Project Scarlet Chaser/Assets/Scripts/NoteObject.cs
+++ Project Scarlet Chaser/Assets/Scripts/NoteObject.cs
@@ -8,6 +8,8 @@ public class NoteObject : MonoBehaviour
 
     public KeyCode keyToPress;
     public KeyCode keyToPress2;
+    public float threshold;
+    public bool dead;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +20,43 @@ public class NoteObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(keyToPress))
+        if (Input.GetKeyDown(keyToPress))
         {
             if(canBePressed)
             {
                 gameObject.SetActive(false);
+                if (GameObject.Find("Button1").GetComponent<Score>().multiplier == 1)
+                {
+                    GameObject.Find("Button1").GetComponent<Score>().victim++;
+                    GameObject.Find("vampire_neutral").GetComponent<Timer>().score++;
+                }
+                if (GameObject.Find("Button1").GetComponent<Score>().multiplier == 2)
+                {
+                    GameObject.Find("Button1").GetComponent<Score>().victim += 2;
+                    GameObject.Find("vampire_neutral").GetComponent<Timer>().score+= 2;
+                }
+                if (GameObject.Find("Button1").GetComponent<Score>().multiplier == 3)
+                {
+                    GameObject.Find("Button1").GetComponent<Score>().victim += 3;
+                    GameObject.Find("vampire_neutral").GetComponent<Timer>().score+= 3;
+                }
+                if (GameObject.Find("Button1").GetComponent<Score>().multiplier == 4)
+                {
+                    GameObject.Find("Button1").GetComponent<Score>().victim += 4;
+                    GameObject.Find("vampire_neutral").GetComponent<Timer>().score+= 4;
+                }
+                if (GameObject.Find("Button1").GetComponent<Score>().multiplier == 5)
+                {
+                    GameObject.Find("Button1").GetComponent<Score>().victim += 5;
+                    GameObject.Find("vampire_neutral").GetComponent<Timer>().score+= 5;
+                }
+                if (GameObject.Find("Button1").GetComponent<Score>().multiplier == 6)
+                {
+                    GameObject.Find("Button1").GetComponent<Score>().victim += 6;
+                    GameObject.Find("vampire_neutral").GetComponent<Timer>().score+= 6;
+                }
+                GameObject.Find("Button1").GetComponent<Score>().hit++;
+                GameObject.Find("vampire_neutral").GetComponent<Timer>().hits++;
             }
         }
 
@@ -31,24 +65,72 @@ public class NoteObject : MonoBehaviour
             if (canBePressed)
             {
                 gameObject.SetActive(false);
+                if (GameObject.Find("Button1").GetComponent<Score>().multiplier == 1)
+                {
+                    GameObject.Find("Button1").GetComponent<Score>().victim++;
+                    GameObject.Find("vampire_neutral").GetComponent<Timer>().score++;
+                }
+                if (GameObject.Find("Button1").GetComponent<Score>().multiplier == 2)
+                {
+                    GameObject.Find("Button1").GetComponent<Score>().victim += 2;
+                    GameObject.Find("vampire_neutral").GetComponent<Timer>().score += 2;
+                }
+                if (GameObject.Find("Button1").GetComponent<Score>().multiplier == 3)
+                {
+                    GameObject.Find("Button1").GetComponent<Score>().victim += 3;
+                    GameObject.Find("vampire_neutral").GetComponent<Timer>().score += 3;
+                }
+                if (GameObject.Find("Button1").GetComponent<Score>().multiplier == 4)
+                {
+                    GameObject.Find("Button1").GetComponent<Score>().victim += 4;
+                    GameObject.Find("vampire_neutral").GetComponent<Timer>().score += 4;
+                }
+                if (GameObject.Find("Button1").GetComponent<Score>().multiplier == 5)
+                {
+                    GameObject.Find("Button1").GetComponent<Score>().victim += 5;
+                    GameObject.Find("vampire_neutral").GetComponent<Timer>().score += 5;
+                }
+                if (GameObject.Find("Button1").GetComponent<Score>().multiplier == 6)
+                {
+                    GameObject.Find("Button1").GetComponent<Score>().victim += 6;
+                    GameObject.Find("vampire_neutral").GetComponent<Timer>().score += 6;
+                }
+                GameObject.Find("Button1").GetComponent<Score>().hit++;
+                GameObject.Find("vampire_neutral").GetComponent<Timer>().hits++;
             }
+        }
+
+        if (transform.position.x < threshold && dead == false)
+        {
+            GameObject.Find("Button1").GetComponent<Score>().multiplier = 1;
+            GameObject.Find("Button1").GetComponent<Score>().hit = 0;
+            gameObject.SetActive(false);
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Activator")
         {
             canBePressed = true;
-           
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other)
+    void OnTriggerExit2D(Collider2D other)
     {
         if (other.tag == "Activator")
         {
             canBePressed = false;
         }
+    }
+
+    void OnEnable()
+    {
+        dead = false;
+    }
+
+    void OnDisable()
+    {
+        dead = true;
     }
 }
--- Project Scarlet Chaser/Assets/Scripts/Score.cs
+++ Project Scarlet Chaser/Assets/Scripts/Score.cs
@@ -8,54 +8,38 @@ public class Score : MonoBehaviour
     public int victim = 0;
     public TMP_Text scoreText;
     public TMP_Text multiplierText;
-    // int multiplier = 1;
-    // const int hitInterval = 10;
+    public int multiplier = 1;
+    int conhit;
+    public int hit = 0;
+    const int hitInterval = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        // multiplier = hitInterval;
-        scoreText.text = "Score: " + victim.ToString();
-       // multiplierText.text = "Multiplier: " + multiplier.ToString();
+        conhit = hitInterval;
+        SetScoreText();
+        SetMultiplierText();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.tag == "Finish")
+        if (hit >= conhit)
         {
-            victim++;
-            scoreText.text = "Score: " + victim.ToString();
-            GameObject.Find("vampire_neutral").GetComponent<Timer>().hits++;
+            multiplier++;
+            conhit += hitInterval;
         }
-           /*    if (victim >= multiplier && multiplier == 1)
-              {
-                  victim++;
-                   multiplierText.text = "Multiplier: " + multiplier.ToString();
-                   multiplier += hitInterval;
-              }
-               if (victim >= multiplier && multiplier == 2)
-                {
-                  victim++;
-                  multiplierText.text = "Multiplier: " + multiplier.ToString();
-                   multiplier += hitInterval;
-               }
-               if (victim >= multiplier && multiplier == 3)
-              {
-                   victim++;
-                  multiplierText.text = "Multiplier: " + multiplier.ToString();
-                   multiplier += hitInterval;
-               }
-            if (victim >= multiplier && multiplier == 4)
-            {
-                victim++;
-                multiplierText.text = "Multiplier: " + multiplier.ToString();
-                multiplier += hitInterval;
-            }
-*/
-        
+        SetScoreText();
+        SetMultiplierText();
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + victim.ToString();
+    }
+
+    void SetMultiplierText()
+    {
+        multiplierText.text = "Multiplier: " + multiplier.ToString();
     }
 }
